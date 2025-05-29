d0939f0796382804b439e20f9c7b1c955a0ba7ed
Pranav <pranavk24h@gmail.com>
2025-05-29 22:11:36 +0530

    minor change in api/apidata/route.ts

--- src/app/api/apidata/route.ts
+++ src/app/api/apidata/route.ts
@@ -1,19 +1,19 @@
-// import path from "path";
-// import { promises as fs } from "fs";
-// import { NextResponse } from "next/server";
+import path from "path";
+import { promises as fs } from "fs";
+import { NextResponse } from "next/server";
 
-// // Named export for the GET method
-// export async function GET(req: Request) {
-//     // Resolve the path to the 'json' folder in the root of the project
-//     const jsonDirectory = path.join(process.cwd(), 'src');
-//     console.log(jsonDirectory);
+// Named export for the GET method
+export async function GET() {
+    // Resolve the path to the 'json' folder in the root of the project
+    const jsonDirectory = path.join(process.cwd(), 'src');
+    console.log(jsonDirectory);
     
-//     // Read the JSON file (e.g., 'data.json') from the directory
-//     const fileContents = await fs.readFile(jsonDirectory + '/blogs.json', 'utf8');
+    // Read the JSON file (e.g., 'data.json') from the directory
+    const fileContents = await fs.readFile(jsonDirectory + '/blogs.json', 'utf8');
     
-//     // Parse the JSON content
-//     const data = JSON.parse(fileContents);
+    // Parse the JSON content
+    const data = JSON.parse(fileContents);
     
-//     // Return the data as a JSON response
-//     return NextResponse.json(data);
-// }
+    // Return the data as a JSON response
+    return NextResponse.json(data);
+}
